5e592027897a3e4776c3838fb68f2fcd4bdbb964
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-11-15 15:29:35 +0100

    add seed for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(name: 'Cyprien Eckert',
+                   email: "some.email@gmail.com",
+                   phone_number: '003348483483',
+                   address: '134 the best address',
+                  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper orci mi, eget tempus urna ullamcorper quis. In lacinia volutpat leo, vel pellentesque nulla fermentum elementum. Cras ullamcorper est at augue vestibulum, quis semper ex molestie. Morbi egestas euismod auctor. Maecenas nisl nunc, molestie in posuere sit amet, euismod a mauris. Pellentesque vel dui imperdiet, posuere turpis pulvinar, luctus massa. Fusce egestas ex non risus pulvinar, in fermentum enim ultrices.")
+user = User.create(name: 'Maxime Mondet',
+                   email: "max.gmail@hotmail.com",
+                   phone_number: '003356473643',
+                   address: '123 carrer de the street',
+                  bio: "Ut dictum justo lobortis erat consequat, in facilisis felis luctus. Curabitur mollis imperdiet elit sed pulvinar.
+Nam suscipit mi quis aliquam suscipit. Pellentesque euismod vehicula massa et finibus. Suspendisse potenti. Integer nibh nulla, pharetra placerat consequat a, blandit a magna.")
+user = User.create(name: 'Lucia Harcegova',
+                   email: "lucia.someeamil@hotmail.com",
+                   phone_number: '0036453728362',
+                   address: 'lewagons stretet 003456',
+                  bio: "Ut dictum justo lobortis erat consequat, in facilisis felis luctus. Curabitur mollis imperdiet elit sed pulvinar.
+Nam suscipit mi quis aliquam suscipit. Pellentesque euismod vehicula massa et finibus. Suspendisse potenti. Integer nibh nulla, pharetra placerat consequat a, blandit a magna.")
