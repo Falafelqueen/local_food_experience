85eb62680d2ef8eec8070473159db477612c4ee3
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-11-15 16:40:59 +0100

    add experience index create and new actions

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,2 +1,29 @@
 class ExperiencesController < ApplicationController
+  # list all experiences
+  def index
+    @experiences = Experience.all
+  end
+
+  # instantiate new experience instance and send it to the experience creation form
+  def new
+    @experience = Experience.all
+  end
+
+  def create
+    # create experience instance with user provided parameters
+    @experience = Experience.new(experience_params)
+    if @experience.save?
+      # redirect user if data provided is valid
+      # redirect_to some_path
+    else
+      # show form again if user data not valid
+      render :new
+    end
+  end
+
+  private
+
+  def experience_params
+    params.require(:experience).permit(:name, :description, :price_per_hour, :min_time, :max_time, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   # devise_for :users
+  devise_for :users
+  # define app root path
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # define experience actions
+  resources :experiences, only: %i[index new create]
 end
