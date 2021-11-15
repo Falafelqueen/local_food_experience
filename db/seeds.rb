9727a0bd8248c8bc9bea19eca63f231a64857a62
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-11-15 15:35:28 +0100

    change to user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,20 @@
-user = User.create(name: 'Cyprien Eckert',
+User.create(name: 'Cyprien Eckert',
                    email: "some.email@gmail.com",
                    phone_number: '003348483483',
                    address: '134 the best address',
-                  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper orci mi, eget tempus urna ullamcorper quis. In lacinia volutpat leo, vel pellentesque nulla fermentum elementum. Cras ullamcorper est at augue vestibulum, quis semper ex molestie. Morbi egestas euismod auctor. Maecenas nisl nunc, molestie in posuere sit amet, euismod a mauris. Pellentesque vel dui imperdiet, posuere turpis pulvinar, luctus massa. Fusce egestas ex non risus pulvinar, in fermentum enim ultrices.")
-user = User.create(name: 'Maxime Mondet',
+                  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper orci mi, eget tempus urna ullamcorper quis. In lacinia volutpat leo, vel pellentesque nulla fermentum elementum. Cras ullamcorper est at augue vestibulum, quis semper ex molestie. Morbi egestas euismod auctor. Maecenas nisl nunc, molestie in posuere sit amet, euismod a mauris. Pellentesque vel dui imperdiet, posuere turpis pulvinar, luctus massa. Fusce egestas ex non risus pulvinar, in fermentum enim ultrices.",
+                  encrypted_password: "rkgjbsdalkfbsafkab")
+User.create(name: 'Maxime Mondet',
                    email: "max.gmail@hotmail.com",
                    phone_number: '003356473643',
                    address: '123 carrer de the street',
-                  bio: "Ut dictum justo lobortis erat consequat, in facilisis felis luctus. Curabitur mollis imperdiet elit sed pulvinar.
-Nam suscipit mi quis aliquam suscipit. Pellentesque euismod vehicula massa et finibus. Suspendisse potenti. Integer nibh nulla, pharetra placerat consequat a, blandit a magna.")
-user = User.create(name: 'Lucia Harcegova',
+                   bio: "Ut dictum justo lobortis erat consequat, in facilisis felis luctus. Curabitur mollis imperdiet elit sed pulvinar.
+Nam suscipit mi quis aliquam suscipit. Pellentesque euismod vehicula massa et finibus. Suspendisse potenti. Integer nibh nulla, pharetra placerat consequat a, blandit a magna.",
+                   encrypted_password: "akwfbsljfvhbweahf")
+User.create(name: 'Lucia Harcegova',
                    email: "lucia.someeamil@hotmail.com",
                    phone_number: '0036453728362',
                    address: 'lewagons stretet 003456',
-                  bio: "Ut dictum justo lobortis erat consequat, in facilisis felis luctus. Curabitur mollis imperdiet elit sed pulvinar.
-Nam suscipit mi quis aliquam suscipit. Pellentesque euismod vehicula massa et finibus. Suspendisse potenti. Integer nibh nulla, pharetra placerat consequat a, blandit a magna.")
+                   bio: "Ut dictum justo lobortis erat consequat, in facilisis felis luctus. Curabitur mollis imperdiet elit sed pulvinar.
+Nam suscipit mi quis aliquam suscipit. Pellentesque euismod vehicula massa et finibus. Suspendisse potenti. Integer nibh nulla, pharetra placerat consequat a, blandit a magna.",
+                   encrypted_password: "askdfbskfbsaf")
