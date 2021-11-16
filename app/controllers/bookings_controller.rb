dce5ab13addf7fb900d5040a7b17606c23d441e3
Maxime Mondet <maxime.mondet@gmail.com>
2021-11-16 15:39:44 +0100

    Adding booking controller methods + routes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(params_booking)
+    @booking.user = current_user
+    @booking.experience = Experience.find(params[:experience_id])
+    if @booking.save
+      redirect_to experience_path(@experience)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_booking
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
