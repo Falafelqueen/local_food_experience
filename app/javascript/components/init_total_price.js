d058a621552907fa396bfe4e86703f6398b7c702
luciaharcegova <fakeluciamail@gmail.com>
2021-11-18 19:22:53 +0100

    functioning total price preview js function

--- app/javascript/components/init_total_price.js
+++ app/javascript/components/init_total_price.js
@@ -1,13 +1,17 @@
 const totalPrice = () => {
   const startDate = document.getElementById("booking_start_date");
   const endDate = document.getElementById("booking_end_date");
+  const pricePreview = document.getElementById("price-preview");
   const updatePrice = () => {
     const startDay = new Date(startDate.value);
     const endDay = new Date(endDate.value);
-    const priceSpan = document.getElementById("price");
-    const price = parseInt(priceSpan.innerHTML);
-    const daysBetween = (endDay - startDay) / (24 * 60 * 60 * 1000);
-    const total = daysBetween * price;
+    if (startDay != "" && endDay != "") {
+      const priceSpan = document.getElementById("price");
+      const price = parseInt(priceSpan.innerHTML);
+      const daysBetween = (endDay - startDay) / (24 * 60 * 60 * 1000);
+      const total = daysBetween * price;
+      pricePreview.innerText = total;
+    }
   };
   startDate.addEventListener("change", updatePrice);
   endDate.addEventListener("change", updatePrice);
