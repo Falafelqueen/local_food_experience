30994cfdf0458fda08c857ca73b7f4785f9d2899
luciaharcegova <fakeluciamail@gmail.com>
2021-11-18 14:36:44 +0100

    added some booking properties - missing views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,28 +1,33 @@
 class BookingsController < ApplicationController
   def show
     @booking = Booking.find(params[:id])
+    @experience = Experience.find(@booking.experience_id)
   end
 
   def new
     @booking = Booking.new
+    @experience = Experience.find(params[:experience_id])
   end
 
   def create
+    @experience = Experience.find(params[:experience_id])
     @booking = Booking.new(params_booking)
     @booking.user = current_user
-    @booking.experience = Experience.find(params[:experience_id])
+    @booking.experience = @experience
+
     if @booking.save
-      redirect_to experience_path(@experience)
+      redirect_to booking_path(@booking)
     else
       render :new
     end
   end
 
+  # def destroy
+
+  # end
+
   private
 
-  def total_price
-    self.total_price = (end_date - start_date) / price_per_day
-  end
 
   def params_booking
     params.require(:booking).permit(:start_date, :end_date)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,13 @@ class UsersController < ApplicationController
       @user.photo.attach(io: picture, filename: "user.png", content_type: "image/png")
       @user.save!
     end
+    @my_experiences = Experience.where(user: params[:id])
+    @my_pending_bookings = Booking.where(status: :active, experience_id: [@my_experiences])
+raise
   end
 end
+
+# list bookings
+# check whether some of my experiences have been booked - get all bookings with status "active" - show those with my experiences
+# confirm/decline booking
+# add a button or function that allows me to change the status of the booking to "archived"
