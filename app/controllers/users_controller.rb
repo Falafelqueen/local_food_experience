87aadd663c2fed05ddb50b0eb257ece73a5d4322
luciaharcegova <fakeluciamail@gmail.com>
2021-11-18 10:30:54 +0100

    added if statement to UserController #show - attaching the default picture to the user if he doesnt have one

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
+require "open-uri"
+
 class UsersController < ApplicationController
   def show
     @user = current_user
+    if @user.photo.attachment.nil?
+      picture = URI.open("https://sdchefs.org/wp-content/uploads/2016/07/chef-placeholder.png")
+      @user.photo.attach(io: picture, filename: "user.png", content_type: "image/png")
+      @user.save!
+    end
   end
 end
