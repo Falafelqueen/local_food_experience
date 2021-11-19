c9b9fa5294e46a8d249f00df83a75b408b5c10a8
luciaharcegova <fakeluciamail@gmail.com>
2021-11-19 15:40:51 +0100

    fixed old-experiences array

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,7 +5,7 @@ class DashboardsController < ApplicationController
     @my_experiences = current_user.experiences
     @my_received_bookings = current_user.received_bookings
     @my_requested_bookings = current_user.bookings.order("start_date")
-    @my_old_bookings = @my_requested_bookings.reject { |booking| booking.end_date.before?(Date.today)}
+    @my_old_bookings = @my_requested_bookings.reject { |booking| booking.end_date.after?(Date.today)}
     @my_current_bookings = @my_requested_bookings.archived.reject{ |booking| booking.start_date.before?(Date.today)}
   end
 end
