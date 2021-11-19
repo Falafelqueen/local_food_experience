4354daa2d3d87d247918690e9b3c8aaa45f21e7f
Cyprien Eckert <cipri.eckert@hotmail.fr>
2021-11-19 11:03:50 +0100

    authorize experience show and fix seeds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: %i[index home]
+  before_action :authenticate_user!, except: %i[index home show]
   include Pundit
 
 
