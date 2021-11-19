7a2544a3028b1a82e9ea344f5a03b3c4a33dd867
Maxime Mondet <maxime.mondet@gmail.com>
2021-11-19 12:57:10 +0100

    new booking page redesign

--- app/javascript/components/init_total_price.js
+++ app/javascript/components/init_total_price.js
@@ -2,15 +2,20 @@ const totalPrice = () => {
   const startDate = document.getElementById("booking_start_date");
   const endDate = document.getElementById("booking_end_date");
   const pricePreview = document.getElementById("price-preview");
+  pricePreview.innerText = "0€"
+
   const updatePrice = () => {
-    const startDay = new Date(startDate.value);
-    const endDay = new Date(endDate.value);
-    if (startDay != "" && endDay != "") {
+    const parsedStartDay = new Date(startDate.value);
+    const parsedEndDay = new Date(endDate.value);
+
+    if (startDate.value != "" && endDate.value != "") {
       const priceSpan = document.getElementById("price");
       const price = parseInt(priceSpan.innerHTML);
-      const daysBetween = (endDay - startDay) / (24 * 60 * 60 * 1000);
+      const daysBetween = (parsedEndDay - parsedStartDay) / (24 * 60 * 60 * 1000);
       const total = daysBetween * price;
-      pricePreview.innerText = total;
+      pricePreview.innerText = total + "€";
+    } else {
+      pricePreview.innerText = "0€"
     }
   };
   startDate.addEventListener("change", updatePrice);
